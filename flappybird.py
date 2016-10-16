6b6647083f095f7dda1a37c974f8dc1539a9666b
Ian Wood <ian.wood108@gmail.com>
2016-10-15 23:05:37 -0400

    Added gap dimension functions

--- flappybird.py
+++ flappybird.py
@@ -5,7 +5,6 @@ from pygame.locals import *  # noqa
 import sys
 import random
 
-
 class FlappyBird:
     def __init__(self):
         # OUR VARIABLES
@@ -20,8 +19,8 @@ class FlappyBird:
         self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
                             pygame.image.load("assets/2.png").convert_alpha(),
                             pygame.image.load("assets/dead.png")]
-        self.wallUp = pygame.image.load("assets/bottom.png").convert_alpha()
-        self.wallDown = pygame.image.load("assets/top.png").convert_alpha()
+        self.lowerPipe = pygame.image.load("assets/bottom.png").convert_alpha()
+        self.upperPipe = pygame.image.load("assets/top.png").convert_alpha()
         self.gap = 160
         self.wallx = 400
         self.birdY = 350
@@ -33,10 +32,25 @@ class FlappyBird:
         self.counter = 0
         self.offset = random.randint(-self.offSetDistance, self.offSetDistance)
 
+    def upperPipeTop(self):
+        return 0 - self.gap - self.offset
+    
+    def upperPipeBottom(self): # also gapTop
+        return 0 - self.gap - self.offset + self.upperPipe.get_height()
+    
+    def lowerPipeTop(self): # also gapBottom
+        return 360 + self.gap - self.offset
+    
+    def pipeLeftSide(self): # also gapLeft
+        return self.wallx + 2
+    
+    def pipeRightSide(self): # also gapRight
+        return self.pipeLeftSide() + self.upperPipe.get_width()
+
     def updateWalls(self):
         self.wallx -= 2
         self.dist += 1
-        if self.wallx < -80:
+        if self.wallx < -self.upperPipe.get_width():
             self.wallx = 400
             self.counter += 1
             self.offset = random.randint(-self.offSetDistance, self.offSetDistance)
@@ -50,19 +64,19 @@ class FlappyBird:
             self.birdY += self.gravity
             self.gravity += 0.2
         self.bird[1] = self.birdY
-        upRect = pygame.Rect(self.wallx,
-                             360 + self.gap - self.offset + 10,
-                             self.wallUp.get_width() - 10,
-                             self.wallUp.get_height())
+        lowerPipeRect = pygame.Rect(self.wallx,
+                             self.lowerPipeTop() + 10,
+                             self.lowerPipe.get_width() - 10,
+                             self.lowerPipe.get_height())
         downRect = pygame.Rect(self.wallx,
-                               0 - self.gap - self.offset - 10,
-                               self.wallDown.get_width() - 10,
-                               self.wallDown.get_height())
-        if upRect.colliderect(self.bird) & self.collisionOn:
+                               self.upperPipeTop() - 10,
+                               self.upperPipe.get_width() - 10,
+                               self.upperPipe.get_height())
+        if lowerPipeRect.colliderect(self.bird) & self.collisionOn:
             self.dead = True
         if downRect.colliderect(self.bird) & self.collisionOn:
             self.dead = True
-        if not 0 < self.bird[1] < 720:
+        if not 0 < self.bird[1] < 720:  
             self.bird[1] = 50
             self.birdY = 50
             self.dead = False
@@ -88,10 +102,10 @@ class FlappyBird:
 
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.background, (0, 0))
-            self.screen.blit(self.wallUp,
-                             (self.wallx, 360 + self.gap - self.offset))
-            self.screen.blit(self.wallDown,
-                             (self.wallx, 0 - self.gap - self.offset))
+            self.screen.blit(self.lowerPipe,
+                             (self.wallx, self.lowerPipeTop()))
+            self.screen.blit(self.upperPipe,
+                             (self.wallx, self.upperPipeTop()))
             self.screen.blit(font.render("pipes: " + str(self.counter),
                                          -1,
                                          (255, 255, 255)),
